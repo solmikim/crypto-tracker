627f7667721111ecbb0ebe0bd91cda7e552151ee
solmikim <umbrellamoss@gmail.com>
2023-07-26 22:57:17 +0900

    Feat : 암호화폐 리스트 및 상세 페이지 구현
    styled components 사용하여 dark/white mode 구현 및 암호화폐 리스트 및 상세 페이지 구현

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import {DefaultTheme} from 'styled-components';
+
+export const theme : DefaultTheme = {
+    bgColor: "#2f3640",
+    textColor: "#f5f6fa",
+    accentColor: "#4cd137",
+};
+
+export const darkTeme : DefaultTheme = {
+    bgColor : "black",
+    textColor : "white",
+    btnColor : "teal"
+}
